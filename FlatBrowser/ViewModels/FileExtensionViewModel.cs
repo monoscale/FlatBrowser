981be824c25e90dc708b621438239db91a3b69e6
mono scale <monoscale@protonmail.com>
2020-12-25 11:08:17 +0100

    Remove uncessary usings

--- FlatBrowser/ViewModels/FileExtensionViewModel.cs
+++ FlatBrowser/ViewModels/FileExtensionViewModel.cs
@@ -1,8 +1,4 @@
 ﻿using FlatBrowser.Models;
-using FlatBrowser.Windows;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FlatBrowser.ViewModels {
     public class FileExtensionViewModel : ViewModelBase {
